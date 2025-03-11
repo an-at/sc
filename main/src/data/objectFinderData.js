395cbcdd29aec52e72e0de2eacddba6cae0a4c62
tsoy.andrey <andrey.tsoy@skyeng.ru>
2025-03-11 15:55:10 +0700

    added dialogs and items hidding

--- main/src/data/objectFinderData.js
+++ main/src/data/objectFinderData.js
@@ -7,14 +7,56 @@ export const items = [
       top: 70 + "%",
       left: 50 + "%",
     },
+    founded: false,
   },
   {
-    name: "recipe book",
-    src: "https://placehold.co/100x100/green/white?text=recip_book",
+    name: "false recipe book",
+    src: "https://placehold.co/100x100/red/white?text=recipe_book",
     position: {
       position: "absolute",
       top: 10 + "%",
-      left: 10 + "%",
+      left: 12 + "%",
+      founded: false,
+    },
+  },
+  {
+    name: "false recipe book",
+    src: "https://placehold.co/100x100/black/white?text=recipe_book",
+    position: {
+      position: "absolute",
+      top: 10 + "%",
+      left: 15 + "%",
+      founded: false,
+    },
+  },
+  {
+    name: "false recipe book",
+    src: "https://placehold.co/100x100/yellow/white?text=recipe_book",
+    position: {
+      position: "absolute",
+      top: 10 + "%",
+      left: 18 + "%",
+      founded: false,
+    },
+  },
+  {
+    name: "false recipe book",
+    src: "https://placehold.co/100x100/brown/white?text=recipe_book",
+    position: {
+      position: "absolute",
+      top: 10 + "%",
+      left: 22 + "%",
+      founded: false,
+    },
+  },
+  {
+    name: "recipe book",
+    src: "https://placehold.co/100x100/green/white?text=recipe_book",
+    position: {
+      position: "absolute",
+      top: 10 + "%",
+      left: 16 + "%",
+      founded: false,
     },
   },
   {
@@ -24,6 +66,7 @@ export const items = [
       position: "absolute",
       top: 15 + "%",
       left: 70 + "%",
+      founded: false,
     },
   },
 ];
@@ -31,14 +74,14 @@ export const tasks = [
   {
     index: 0,
     description:
-      "Before we start, I need my glasses because I can’t see you well right now.",
+      "First of all, I need my… I mean my… Oh gosh, I forgot what it’s called! It’s a special device for my eyes to see every imperfection in the world",
     itemName: "glasses",
     isCompleted: false,
   },
   {
     index: 1,
     description:
-      "Oh, you are quite an attractive little boy! Remind me again, why are you here? Ohh, yeah, your waterboard. I think I heard something from my friend, but honestly, I’m too busy to manage that right now. I need to switch off the stove and do chores at the same time. Oh, I have an idea! Could you help me find some things while my dinner is cooking? Let's start with my recip book",
+      "Thank God I can see you now! Honestly, sweetie, without my glasses, you looked more attractive. Anyway, we need to continue. I need my special book with the secret ingredient for traditional Italian pasta. It’s the color of pure pleasure, you will find it quick. Could you bring it to me please? By the way, I forgot to set a time and you are tricky girl didn't recognize me about that",
     itemName: "recipe book",
     isCompleted: false,
   },
@@ -50,9 +93,13 @@ export const tasks = [
   },
 ];
 export const dialogueTexts = [
-  "Oh, hello, sweetie. What’s your name again? Hmm, what an unusual name!",
-  "So, what are you looking for here? Oh, your surfboard? Sure, it’s an extremely useful thing for school. I remember myself at your age in school—I was such a beautiful girl.",
-  "text3",
-  "text4",
-  "text5",
+  "Oh, hello, sweetie. What’s your name again? Hmm, what an unusual name for a girl! Oh, you’re a boy? Sorry, I didn’t realize. By the way, why are you here?",
+  "You’re looking for a surfboard? Sure, it’s an extremely useful thing for school. You can draw on it and leave notes for your classmates. I remember myself at your age in school—I was such a beautiful girl.",
+  "I was the top student at my school, and everybody liked me until the accident with the apple. I was eating in the school cafeteria when something hit me on the forehead. You might think it was a brilliant idea, but it was actually a big red apple. Since that accident, I’ve been making mistakes with words.",
+  "Ohh, sorry! You don’t have enough time for my story and just want to find a board to draw something on? What did you say? You need that board to surf? You’re a weirdo, but it’s your choice.",
+  "You know what else is weird? My school story with the apple. Let me tell you, I was the top student at my school, and…",
+  "Ohh, God, don’t shout at me, little girl! What do you need, by the way?",
+  "A board? I don’t know anything about a board, but you just reminded me about my dinner. I was cutting a cucumber on a cutting board before you showed up here.",
+  "Honestly, I don’t have much time for you and your board issue. I need to prepare my lunch and do some chores, so if you’re not going to help me, you should leave.",
+  "For real, you could help me with the chores. What a brilliant idea! So, if you manage everything in five minutes, I will have enough time to answer your questions",
 ];
--- main/src/features/objectFinder/components/Game.js
+++ main/src/features/objectFinder/components/Game.js
@@ -9,6 +9,8 @@ import Npc from "./Npc.js";
 
 /*
 Вопросы
+1. не оч понял коммент про то что нужно стопать таймер, он же и так стопается у меня когда время выходит или задания кончаются, в каких кейсах его еще стопать нужно
+2. у меня изменение items после того как их находят происходит в самом объекте, то есть я снова мутирую текущие значения а не делаю новые, но вроде как все работает, нужно все таки вынести это в какой-то отдельный стейт?
 */
 
 export default function Game() {
@@ -36,7 +38,6 @@ export default function Game() {
       intervalId.current = null;
     }
     if (uncompletedTasks.length === 0 && currentTime > 0) {
-      setState("success"); // временно сменил чтобы проверить отображения текста про выполненность заданий
       clearInterval(intervalId.current);
       intervalId.current = null;
     }
@@ -57,6 +58,7 @@ export default function Game() {
     if (currentTask.itemName === itm.name) {
       const updatedList = uncompletedTasks.slice(1);
       setUncompletedTasks(updatedList);
+      itm.founded = true; // вот в этом месте из вопроса №2 я просто меняю значение в объекте в текущем массиве, а не создаю новый каждый раз, это не ок наверное?
     }
   }
 
@@ -114,21 +116,23 @@ export default function Game() {
       <>
         <div className="room">
           <div className="npcAndDialogue">
-            <Npc />
+            <Npc option={"glasses"} />
             <Task currentTask={currentTask} clickHandler={clickHandler} />
           </div>
           <Timer time={currentTime} />
-          {items.map((itm) => (
-            <Item
-              key={itm.name}
-              name={itm.name}
-              imgSrc={itm.src}
-              position={itm.position}
-              clickHandler={() => {
-                clickHandler(itm);
-              }}
-            />
-          ))}
+          {items
+            .filter((itm) => !itm.founded)
+            .map((itm) => (
+              <Item
+                key={itm.name}
+                name={itm.name}
+                imgSrc={itm.src}
+                position={itm.position}
+                clickHandler={() => {
+                  clickHandler(itm);
+                }}
+              />
+            ))}
         </div>
       </>
     );
@@ -138,34 +142,25 @@ export default function Game() {
       <>
         <div className="room">
           <div className="npcAndDialogue">
-            <Npc />
+            <Npc option={"glasses"} />
             <div className="task"> Все задания выполнены</div>
           </div>
           <Timer time={currentTime} />
-          {items.map((itm) => (
-            <Item
-              key={itm.name}
-              name={itm.name}
-              imgSrc={itm.src}
-              position={itm.position}
-            />
-          ))}
+          {items
+            .filter((itm) => !itm.founded)
+            .map((itm) => (
+              <Item
+                key={itm.name}
+                name={itm.name}
+                imgSrc={itm.src}
+                position={itm.position}
+                clickHandler={() => {
+                  clickHandler(itm);
+                }}
+              />
+            ))}
         </div>
       </>
     );
   }
-  // if (state === "success") {
-  //   return (
-  //     <>
-  //       <p> You won</p>
-  //     </>
-  //   );
-  // }
-  // if (state === "fail") {
-  //   return (
-  //     <>
-  //       <p> You failed</p>
-  //     </>
-  //   );
-  // }
 }
